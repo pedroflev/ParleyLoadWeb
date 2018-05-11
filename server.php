2a4476c8fb4eaafb97c8ea6ac758d8f7c863ebd7
Pedro Fonseca <pedrof.lev@gmail.com>
2018-05-11 13:15:01 -0500

    Add files via upload

---
+++ server.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_POST["password"])){
+	$password = $_POST["password"];
+	$login = $_POST(["login"]);
+
+	$database = file_get_contents("database.txt");
+
+	$pairs = explode("-", $database);
+
+	for($i=0;$i<3;$i++){
+		$user =explode(":", $pairs[$i]);
+		if (($user[0] == $login) && ($user[1] == $password)){
+			echo "Bienvenido,". $user[0];
+			$loggedin = 1;
+		}
+
+	}
+
+	if ($loggedin != 1) {
+		echo "Usuario o contraseña incorrecto";
+	}
+}
+
+?>
